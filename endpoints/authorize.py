4aac450fd416a71adcc9fcce585211fc585f530e
Nikolay Makarchuk <makarchuk93@gmail.com>
2025-08-04 21:08:07 +0300

    delete authorize repeating from authorize.py

--- endpoints/authorize.py
+++ endpoints/authorize.py
@@ -3,14 +3,6 @@ from endpoints.base_endpoint import BaseEndpoint
 
 class AuthorizeEndpoint(BaseEndpoint):
 
-    def authorize(self, name):
-        self.logger.info(f"Авторизация пользователя: '{name}'")
-        self.last_response = requests.post(
-            f"{self.base_url}/authorize", json={"name": name}
-        )
-        self.last_data = self.last_response.json()
-        return self
-
     def post_authorize(self, json=None):
         self.logger.info(f"POST /authorize с json={json}")
         self.last_response = requests.post(
